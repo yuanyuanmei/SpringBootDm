46c10b66705157460cda20c91966158b748a9e9a
yuanyuanmei <46510949+yuanyuanmei@users.noreply.github.com>
2019-06-27 18:01:05 +0800

    Update BaseEntity.java
    common

--- src/main/java/com/example/common/base/BaseEntity.java
+++ src/main/java/com/example/common/base/BaseEntity.java
@@ -1,6 +1,9 @@
 package com.example.common.base;
 
 import com.alibaba.fastjson.annotation.JSONField;
+import com.example.common.annotations.sql.Column;
+import com.example.common.annotations.sql.Exclude;
+import com.example.common.annotations.sql.Id;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -10,8 +13,10 @@ import java.util.Date;
 @Data
 public abstract class BaseEntity<ID extends Serializable> implements Serializable {
 
+    @Exclude
     private static final long serialVersionUID = 1L;
 
+    @Id
     private ID id;
 
     @JSONField
@@ -23,5 +28,6 @@ public abstract class BaseEntity<ID extends Serializable> implements Serializabl
     private Date updateTime;
 
     @JSONField
+    @Column(name = "is_delete")
     private Integer deleteStatus;
 }
