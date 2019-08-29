d506fa2c536cf3883e8081c87d8984f738b07a6b
yuanyuanmei <46510949+yuanyuanmei@users.noreply.github.com>
2019-08-29 16:46:44 +0800

    Update ReflectUtil.java
    dmdmdmdm

--- src/main/java/com/example/common/ReflectUtil.java
+++ src/main/java/com/example/common/ReflectUtil.java
@@ -1,11 +1,13 @@
 package com.elan.bg.utils;
 
 import com.elan.bg.dto.LineDto;
+import org.apache.commons.lang.StringUtils;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class ReflectUtil {
 
@@ -30,9 +32,9 @@ public class ReflectUtil {
      * @param object
      * @return
      */
-    public static List<Object> getListObject(Object object,String splitChar) {
+    public static <T> List<T> getListObject(T object,String splitChar) {
             Field[] fields = object.getClass().getDeclaredFields();
-            List<Object> list = new ArrayList<>();
+            List<T> list = new ArrayList<>();
             try {
                 for (int i = 0; i < fields.length; i++) {
 
@@ -51,15 +53,53 @@ public class ReflectUtil {
             return list;
         }
 
-        public static void addList(List<Object> list,String value,Field field,Object newInstance,int index){
+        public static <T> void addList(List<T> list,String value,Field field,Object newInstance,int index){
             try {
                 Object o = list.size() > 0 && list.size() > index ? list.get(index): newInstance;
                 field.set(o,value);
-                if (list.size() <= index) list.add(o);
+                if (list.size() <= index) list.add((T) o);
             } catch (Exception e) {
                 e.printStackTrace();
             }
 
         }
+
+    /**
+     * 将集合组合成一个对象
+     * @param list
+     * @param splitChar
+     * @param <T>
+     * @return
+     */
+    public static <T> Object getObject(List<T> list,String splitChar){
+        if(list.size() == 0){
+            return null;
+        }
+        try {
+            Object item = list.get(0).getClass().newInstance();
+            Field[] fields = item.getClass().getDeclaredFields();
+            for(int i = 0;i< fields.length; i++){
+                fields[i].setAccessible(true);
+                for(Object obj:list){
+                    Field[] objfileds = obj.getClass().getDeclaredFields();
+                    for(int j=0;j<objfileds.length;j++){
+                        objfileds[j].setAccessible(true);
+                        if(fields[i].getName().equals(objfileds[j].getName())){
+                            Object value = objfileds[j].get(obj);
+                            Object itemvalue = fields[i].get(item);
+                            if(value instanceof String){
+                                fields[i].set(item,itemvalue == null ? value:itemvalue+splitChar+value);
+                            }
+                        }
+                    }
+                }
+            }
+            return item;
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
 
 }
